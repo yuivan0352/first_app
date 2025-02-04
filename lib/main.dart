77aa1860bd10e265c81228e8a62d53eaa4e2aab1
Ivan Yu <yu.ivan0352@gmail.com>
2025-02-04 11:59:20 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 9, 1, 24)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+                "Hello World",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
